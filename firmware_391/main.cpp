59077ac12fbc21bb6af62e716edb5308a93bd867
Tyler Keeling <tylerwkeeling@gmail.com>
2021-02-28 15:49:13 -0700

    Scrambled bytes from Python bluetooth

--- firmware_391/main.cpp
+++ firmware_391/main.cpp
@@ -11,19 +11,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "rs232.h"
+#include "parser.h"
+
+RS232 bluetooth(0xFF200080);
+
+__irq void IRQHandler (void)
+{
+    volatile unsigned int *base = (unsigned int *) 0x80000000;
+
+    unsigned char c;
+    bluetooth.getchar(&c);
+    printf("IRQ Char: %02x", c);
+    //if (*base == 3)          // which interrupt was it?
+    //{
+    //    bluetooth.getchar();     // process the interrupt
+    //}
+    *(base+1) = 0;           // clear the interrupt
+}
+
 
 int main(void) {
-
-    RS232 bluetooth;
     // send and receive hello world
     bluetooth.putchar((int) 'C');
     bluetooth.putchar((int) 'P');
     bluetooth.putchar((int) 'E');
     bluetooth.putchar((int) 'N');
 
+    char *data;
+    Parser parser(&bluetooth);
     // print PEN
     while(1) {
-        bluetooth.getchar();
+        parser.getdata(*&data);
     }
 
 	return 0;
--- firmware_391/rs232.cpp
+++ firmware_391/rs232.cpp
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* 8 byte wide (0x0080-0x0087).
+/*
  * Find Atlera guide "RS232 UART for Altera DE-Series Boards"
  * For programming model and registers
- */
-#define UART_data    0xFF200080;
-//#define UART_control (*(volatile unsigned int *)0xFF200084);
-
-//volatile unsigned int *UART_data = 0xFF200080;
-
-/*
+ *
+ * The module is 16-byte memory mapped (for example, 0x80-0x87)
+ * Data reg is 8 byte wide             (for example, 0x80-0x83)
+ * Control reg is 8 bytes wide         (for example, 0x84-0x87)
+ *
  * ARM is little-endian so the lsb of the 32-bit word is at 0x80.
  * When we load the data in address UART_data, we load 4 memory addresses
  * (each 8 bits wide) into a 32-bit wide register
  */
+
+RS232::RS232(unsigned int address) {
+	this->address = address;
+	this->control_address = address+4;
+
+	// enable read interrupts
+	*(volatile unsigned int *)control_address = 0x01;
+}
 
 int RS232::putchar(char c)
 {
@@ -25,33 +31,34 @@ int RS232::putchar(char c)
 	printf("Putting char %c\n", c);
 
 	/* write character to Transmitter fifo register */
-	*(volatile unsigned int *)0xFF200080 = c;
+	*(volatile unsigned int *)address = c;
 
 	return c;
 }
 
-int RS232::getchar()
+int RS232::getchar(unsigned char *c)
 {
 
-	unsigned int data = *(volatile unsigned int *)0xFF200080;
+	unsigned int data = *(volatile unsigned int *)address;
 	unsigned int data_valid = data & 0x8000;
 	unsigned int chars_left = (data >> 16) & 0xFF;
 
-	// byte 15 of data is the "RVALID" flag. Check this.
-	if (data_valid && chars_left) {
-		printf("%d characters remaining in the read FIFO ", chars_left);
-		printf("Data field is valid, ");
-		printf("returning char %c\n", (data & 0xFF));
+	unsigned int control = *(volatile unsigned int *)control_address;
 
-		// I'm pretty sure I'm doing parity wrong
-		if (!(data & 0200)){
-			printf("But there is a parity error. Disregard\n");
-		}
-		return data & 0xFF;
+	*c = data & 0xFF;
+	// byte 15 of data is the "RVALID" flag. Check this.
+	if (data_valid) {
+		//printf("%d characters remaining in the read FIFO ", chars_left);
+		//printf("Data field is valid, ");
+		//printf("returning char %02x\n", *c);
+
+		// Bluetooth chip has no parity so we can ignore that.
+		// We will CRC32 our data instead.
+		return 0;
 	}
 
 	// return only the last byte of the word
-	return 0;
+	return -1;
 }
 
 int RS232::received_data(void)
